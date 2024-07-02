1a2c81d27cf5616c41cf112154b4787a64c9c087
ydilmoni <yonidil1997@gmail.com>
2024-07-02 14:06:02 +0300

    Order work for binart tree

--- Tree.hpp
+++ Tree.hpp
@@ -159,7 +159,7 @@ namespace ariel
                     return nullptr;
                 }
 
-                Node<T, k>* current = dfsStack.top();
+                Node<T, k> *current = dfsStack.top();
                 dfsStack.pop();
 
                 for (int i = current->getNumOfChildren() - 1; i >= 0; i--)
@@ -173,6 +173,108 @@ namespace ariel
             }
         }; // class DFS iterator
 
+        class PostOrderIterator
+        {
+        private:
+            stack<Node<T, 2> *> s1, s2;
+
+        public:
+            PostOrderIterator(Node<T, 2> *root)
+            {
+                if (root != nullptr)
+                {
+                    s1.push(root);
+                    while (!s1.empty())
+                    {
+                        Node<T, 2> *node = s1.top();
+                        s1.pop();
+                        s2.push(node);
+                        if (node->children[0] != nullptr)
+                            s1.push(node->children[0]);
+                        if (node->children[1] != nullptr)
+                            s1.push(node->children[1]);
+                    }
+                }
+            }
+
+            bool hasNext() const
+            {
+                return !s2.empty();
+            }
+
+            Node<T, 2> *next()
+            {
+                if (!hasNext())
+                    return nullptr;
+                Node<T, 2> *node = s2.top();
+                s2.pop();
+                return node;
+            }
+        }; // class PostOrderIterator
+
+        class PreOrderIterator
+        {
+        private:
+            stack<Node<T, 2> *> s;
+
+        public:
+            PreOrderIterator(Node<T, 2> *root)
+            {
+                if (root != nullptr)
+                    s.push(root);
+            }
+
+            bool hasNext() const
+            {
+                return !s.empty();
+            }
+
+            Node<T, 2> *next()
+            {
+                if (!hasNext())
+                    return nullptr;
+                Node<T, 2> *node = s.top();
+                s.pop();
+                if (node->children[1] != nullptr)
+                    s.push(node->children[1]);
+                if (node->children[0] != nullptr)
+                    s.push(node->children[0]);
+                return node;
+            }
+        }; // class PreOrderIterator
+
+        class InOrderIterator
+        {
+        private:
+            stack<Node<T, 2> *> s;
+            Node<T, 2> *current;
+
+        public:
+            InOrderIterator(Node<T, 2> *root) : current(root) {}
+
+            bool hasNext() const
+            {
+                return !s.empty() || current != nullptr;
+            }
+
+            Node<T, 2> *next()
+            {
+                while (current != nullptr)
+                {
+                    s.push(current);
+                    current = current->children[0];
+                }
+
+                if (s.empty())
+                    return nullptr;
+
+                Node<T, 2> *node = s.top();
+                s.pop();
+                current = node->children[1];
+                return node;
+            }
+        }; // class InOrderIterator
+
         BFSIterator bfs_begin()
         {
             return BFSIterator(root);
@@ -181,6 +283,33 @@ namespace ariel
         DFSIterator dfs_begin()
         {
             return DFSIterator(root);
+        }
+
+        PostOrderIterator post_order_begin()
+        {
+            if constexpr (k != 2)
+            {
+                throw std::runtime_error("Post-order traversal is only supported for binary trees (k=2)");
+            }
+            return PostOrderIterator(root);
+        }
+
+        PreOrderIterator pre_order_begin()
+        {
+            if constexpr (k != 2)
+            {
+                throw std::runtime_error("Pre-order traversal is only supported for binary trees (k=2)");
+            }
+            return PreOrderIterator(root);
+        }
+
+        InOrderIterator in_order_begin()
+        {
+            if constexpr (k != 2)
+            {
+                throw std::runtime_error("In-order traversal is only supported for binary trees (k=2)");
+            }
+            return InOrderIterator(root);
         }
 
     }; // class Tree
--- main.cpp
+++ main.cpp
@@ -8,48 +8,86 @@ using namespace std;
 
 int main()
 {
-    // Create 12 int nodes
-    Node<int, 3> *node1 = new Node<int, 3>(1);
-    Node<int, 3> *node2 = new Node<int, 3>(2);
-    Node<int, 3> *node3 = new Node<int, 3>(3);
-    Node<int, 3> *node4 = new Node<int, 3>(4);
-    Node<int, 3> *node5 = new Node<int, 3>(5);
-    Node<int, 3> *node6 = new Node<int, 3>(6);
-    Node<int, 3> *node7 = new Node<int, 3>(7);
-    Node<int, 3> *node8 = new Node<int, 3>(8);
-    Node<int, 3> *node9 = new Node<int, 3>(9);
-    Node<int, 3> *node10 = new Node<int, 3>(10);
+    // Create 15 int nodes for binary tree
+    Node<int> *root = new Node<int>(1);
+    Node<int> *node1 = new Node<int>(2);
+    Node<int> *node2 = new Node<int>(3);
+    Node<int> *node3 = new Node<int>(4);
+    Node<int> *node4 = new Node<int>(5);
+    Node<int> *node5 = new Node<int>(6);
+    Node<int> *node6 = new Node<int>(7);
+    Node<int> *node7 = new Node<int>(8);
+    Node<int> *node8 = new Node<int>(9);
+    Node<int> *node9 = new Node<int>(10);
+    Node<int> *node10 = new Node<int>(11);
+    Node<int> *node11 = new Node<int>(12);
+    Node<int> *node12 = new Node<int>(13);
+    Node<int> *node13 = new Node<int>(14);
+    Node<int> *node14 = new Node<int>(15);
 
-    node3->addChild(node2);
-    node3->addChild(node4);
-    node3->addChild(node5);
-    node2->addChild(node1);
-    node2->addChild(node6);
-    node4->addChild(node7);
-    node4->addChild(node8);
-    node5->addChild(node9);
-    node9->addChild(node10);
+    // Connect nodes
+
+    Tree<int> tree(root);
+    tree.add_sub_node(root, node1);
+    tree.add_sub_node(root, node2);
+    tree.add_sub_node(node1, node3);
+    tree.add_sub_node(node1, node4);
+    tree.add_sub_node(node2, node5);
+    tree.add_sub_node(node2, node6);
+    tree.add_sub_node(node3, node7);
+    tree.add_sub_node(node3, node8);
+    tree.add_sub_node(node4, node9);
+    tree.add_sub_node(node4, node10);
+    tree.add_sub_node(node5, node11);
+    tree.add_sub_node(node5, node12);
+    tree.add_sub_node(node6, node13);
+    tree.add_sub_node(node6, node14);
 
     // Create a Tree<int, 3> object and try to put node8 as root.
-    Tree<int, 3> tree;
-    tree.add_root(node10);
 
     // BFS traversal
-    Tree<int, 3>::BFSIterator bfs = tree.bfs_begin();
+    Tree<int>::BFSIterator bfs = tree.bfs_begin();
     while (bfs.hasNext())
     {
-        Node<int, 3> *node = bfs.next();
+        Node<int> *node = bfs.next();
         cout << node->getValue() << endl;
     }
 
-    tree.print_tree();
+    // tree.print_tree();
 
-    Tree<int, 3>::DFSIterator dfs = tree.dfs_begin();
+    Tree<int>::DFSIterator dfs = tree.dfs_begin();
     while (dfs.hasNext())
     {
-        Node<int, 3>* node = dfs.next();
+        Node<int> *node = dfs.next();
         cout << node->getValue() << endl;
     }
+
+    // Post-order traversal
+    Tree<int, 2>::PostOrderIterator post_it = tree.post_order_begin();
+    while (post_it.hasNext())
+    {
+        Node<int, 2> *node = post_it.next();
+        cout << node->getValue() << " ";
+    }
+    cout << endl;
+
+    // Pre-order traversal
+    Tree<int, 2>::PreOrderIterator pre_it = tree.pre_order_begin();
+    while (pre_it.hasNext())
+    {
+        Node<int, 2> *node = pre_it.next();
+        cout << node->getValue() << " ";
+    }
+    cout << endl;
+
+    // In-order traversal
+    Tree<int, 2>::InOrderIterator in_it = tree.in_order_begin();
+    while (in_it.hasNext())
+    {
+        Node<int, 2> *node = in_it.next();
+        cout << node->getValue() << " ";
+    }
+    cout << endl;
 
     return 0;
 }
